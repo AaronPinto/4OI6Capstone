8121b4dedcc6e6204e8db5cc1acd887d124fb8c6
Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>
2023-04-11 04:55:56 -0400

    add initial byte handling
    Signed-off-by: Aaron Pinto <10177767+AaronPinto@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "ctre/phoenix/cci/Unmanaged_CCI.h"
 #include "ctre/phoenix/platform/Platform.h"
 #include "ctre/phoenix/unmanaged/Unmanaged.h"
+#include <arpa/inet.h>
 #include <chrono>
 #include <fcntl.h>
 #include <iostream>
@@ -13,7 +14,6 @@
 #include <sys/socket.h>
 #include <thread>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
@@ -21,6 +21,10 @@ using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 
 TalonSRX cimMotor(1); // Use the specified interface
+
+void configureMotor() {
+    cimMotor.ConfigOpenloopRamp(2.0);
+}
 
 void drive(double fwd) {
     cimMotor.Set(ControlMode::PercentOutput, fwd);
@@ -34,7 +38,8 @@ void sleepApp(int ms) {
 
 int create_connect_socket() {
     int server_socket, client_socket;
-    struct sockaddr_in server_address, client_address;
+    struct sockaddr_in server_address {};
+    struct sockaddr_in client_address {};
     socklen_t client_address_size = sizeof(client_address);
     char buffer[BUFFER_SIZE];
 
@@ -47,7 +52,7 @@ int create_connect_socket() {
     server_address.sin_port = htons(50001);
 
     // bind the socket to the server address
-    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
     // listen for incoming connections
     listen(server_socket, 5);
@@ -55,60 +60,69 @@ int create_connect_socket() {
     printf("Listening on port %d\n", ntohs(server_address.sin_port));
 
     // accept the client connection
-    client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_size);
+    client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_address_size);
 
     printf("Accepted connection from %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
 
     return client_socket;
 }
 
-int receive_byte(int client_socket, char* buffer) {
+int receive_byte(int client_socket, char *buffer) {
     // receive a single byte of data
     int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
 
     if (bytes_received < 0) {
         // handle error
         perror("Error receiving data");
-        exit(1);
+        return -1;
     } else if (bytes_received == 0) {
         // handle connection closed by remote peer
         printf("Connection closed by remote peer\n");
-        exit(1);
+        return -1;
     } else {
         // do something with the received data
-        printf("Received %d bytes of data: %d\n", bytes_received, buffer[0]-48);
-        return (buffer[0]-48);
+        printf("Received %d bytes of data: %d\n", bytes_received, buffer[0] - 48);
+        return buffer[0] - 48;
     }
 }
 
 
 int main() {
+    drive(0.0);
+
     int client_socket = create_connect_socket();
     char buffer[BUFFER_SIZE];
 
     while (true) {
-        // receive a single byte of data
-        // int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-
-        // if (bytes_received < 0) {
-        //     // handle error
-        //     perror("Error receiving data");
-        //     exit(1);
-        // } else if (bytes_received == 0) {
-        //     // handle connection closed by remote peer
-        //     printf("Connection closed by remote peer\n");
-        //     exit(1);
-        // } else {
-        //     // do something with the received data
-        //     printf("Received %d bytes of data: %d\n", bytes_received, buffer[0]-48);
-        // }
-
         int bytes_received = receive_byte(client_socket, buffer);
+
+        switch (bytes_received) {
+            case 0:
+                std::cout << "Burst" << std::endl;
+                break;
+            case 1:
+                std::cout << "Burst++" << std::endl;
+                break;
+            case 2:
+                std::cout << "Uphill" << std::endl;
+                break;
+            case 3:
+                std::cout << "Uphill++" << std::endl;
+                break;
+            case 4:
+                std::cout << "STOP" << std::endl;
+                drive(0.0);
+                break;
+            default:
+                std::cout << "default case" << std::endl;
+                break;
+        }
+
+        sleepApp(20);
     }
 
     // close the sockets
     close(client_socket);
-    // close(server_socket);
 
     return 0;
 }
